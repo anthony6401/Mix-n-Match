04c03e21ba737e8bce6f2dcb302c1e018657a0c2
anthony6401 <65406233+anthony6401@users.noreply.github.com>
2021-02-02 11:25:26 +0800

    Delete BotIdentity.java

--- src/main/java/bot/utility/BotIdentity.java
+++
@@ -1,6 +0,0 @@
-package bot.utility;
-
-public class BotIdentity {
-    public static String username = "MixnMatchBot";
-    public static String botToken = "1242060435:AAHiEreNvJ5Qw3cud-TOef7IULiUwz8wpxg";
-}
