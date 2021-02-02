f514a69a43da964a40ccd414c9bac116065b77a9
anthony6401 <65406233+anthony6401@users.noreply.github.com>
2021-02-02 11:26:06 +0800

    Delete GoogleMapKeyAndURL.java

--- src/main/java/bot/utility/GoogleMapKeyAndURL.java
+++
@@ -1,7 +0,0 @@
-package bot.utility;
-
-public class GoogleMapKeyAndURL {
-    public static String key = "AIzaSyDIWEKaDIthMAPmibgP4PEIUuNeCP69fi0";
-    public static String findPlaceFromString = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
-    public static String findCoordinates = "https://maps.googleapis.com/maps/api/geocode/json";
-}
